647c428d375f3383f9179a215f70fd586bdfc4d6
Rysya <pbl6a@bk.ru>
2026-05-18 12:14:24 +0300

    Initial Commit

---
+++ List of guests/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  List of guests
+//
+//  Created by Мария Александрова on 18.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
